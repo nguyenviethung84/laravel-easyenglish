51ba2c59e21c92ed76f3b674339eb03a1e0297cc
hungit <hungnv@hamogroups.com>
2023-06-06 07:12:21 +0700

    show tinymce

--- resources/views/post/form.blade.php
+++ resources/views/post/form.blade.php
@@ -1,3 +1,5 @@
+<script src="{{ asset('js/app.js') }}"></script>
+
 <div class="box box-info padding-1">
     <div class="box-body">
         
@@ -26,11 +28,22 @@
             {{ Form::text('excerpt', $post->excerpt, ['class' => 'form-control' . ($errors->has('excerpt') ? ' is-invalid' : ''), 'placeholder' => 'Excerpt']) }}
             {!! $errors->first('excerpt', '<div class="invalid-feedback">:message</div>') !!}
         </div>
-        <div class="form-group">
+        <!-- <div class="form-group">
             {{ Form::label('body') }}
             {{ Form::text('body', $post->body, ['class' => 'form-control' . ($errors->has('body') ? ' is-invalid' : ''), 'placeholder' => 'Body']) }}
             {!! $errors->first('body', '<div class="invalid-feedback">:message</div>') !!}
+        </div> -->
+
+        <div class="form-group">
+            {{ Form::label('body') }}
+            <!-- {{ Form::textarea('body', $post->body, ['class' => 'form-control' . ($errors->has('body') ? ' is-invalid' : ''), 'placeholder' => 'Body', 'rows' => 10, 'cols' => 50]) }} -->
+            <textarea class="form-control richTextBox" name="body" id="body">
+                {{ $post->body }}
+            </textarea>
+            
+            {!! $errors->first('body', '<div class="invalid-feedback">:message</div>') !!}
         </div>
+        
 
         <div class="form-group">
             {{ Form::label('image') }}
@@ -74,3 +87,59 @@
         <button type="submit" class="btn btn-primary">{{ __('Submit') }}</button>
     </div>
 </div>
+
+
+<script src="http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.js"></script>
+
+
+@push('javascript')
+    <script>
+        $(document).ready(function() {
+            // var route_prefix = "http://localhost/laravel-filemanager/";
+            // $('#lfm').filemanager('image', {prefix: route_prefix});
+            // var editor_config = {
+            //     path_absolute : "/",
+            //     selector: 'textarea.richTextBox[name="abc"]',
+            //     height:350,
+            //     relative_urls: false,
+            //     plugins: [
+            //     "advlist autolink lists link image charmap print preview hr anchor pagebreak",
+            //     "searchreplace wordcount visualblocks visualchars code fullscreen",
+            //     "insertdatetime media nonbreaking save table directionality",
+            //     "emoticons template paste textpattern"
+            //     ],
+            //     toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image media",
+            //     file_picker_callback : function(callback, value, meta) {
+            //     var x = window.innerWidth || document.documentElement.clientWidth || document.getElementsByTagName('body')[0].clientWidth;
+            //     var y = window.innerHeight|| document.documentElement.clientHeight|| document.getElementsByTagName('body')[0].clientHeight;
+
+            //     var cmsURL = editor_config.path_absolute + 'TinyMCE5/laravel-filemanager?editor=' + meta.fieldname;
+            //     if (meta.filetype == 'image') {
+            //         cmsURL = cmsURL + "&type=Images";
+            //     } else {
+            //         cmsURL = cmsURL + "&type=Files";
+            //     }
+
+            //     tinyMCE.activeEditor.windowManager.openUrl({
+            //         url : cmsURL,
+            //         title : 'Filemanager',
+            //         width : x * 0.8,
+            //         height : y * 0.8,
+            //         resizable : "yes",
+            //         close_previous : "no",
+            //         onMessage: (api, message) => {
+            //         callback(message.content);
+            //         }
+            //     });
+            //     }
+            // };
+            // tinymce.init(window.voyagerTinyMCE.getConfig(editor_config));
+
+            var additionalConfig = {
+                selector: 'textarea.richTextBox[name="body"]',
+            }
+            $.extend(additionalConfig, {!! json_encode($options->tinymceOptions ?? '{}') !!})
+            tinymce.init(window.voyagerTinyMCE.getConfig(additionalConfig));
+        });
+    </script>
+@endpush
