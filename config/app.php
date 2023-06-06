12670f1cf31c94b65cfc26089cefb76810c3abef
hungit <hungnv@hamogroups.com>
2023-06-06 07:13:06 +0700

    sử dụng mix và laravel manager

--- config/app.php
+++ config/app.php
@@ -195,6 +195,8 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         Collective\Html\HtmlServiceProvider::class,
+        UniSharp\LaravelFilemanager\LaravelFilemanagerServiceProvider::class,
+        Intervention\Image\ImageServiceProvider::class,
 
     ],
 
@@ -213,6 +215,7 @@ return [
         // 'ExampleClass' => App\Example\ExampleClass::class,
         'Form' => Collective\Html\FormFacade::class,
         'Html' => Collective\Html\HtmlFacade::class,
+        'Image' => Intervention\Image\Facades\Image::class,
     ])->toArray(),
 
 ];
---
+++ webpack.mix.js
@@ -0,0 +1,15 @@
+let mix = require('laravel-mix');
+
+// mix.js('src/app.js', 'dist').setPublicPath('dist');
+
+// mix.js([
+//     'resources/js/app.js',
+//     'node_modules/tinymce/tinymce.min.js',
+// ], 'public/js/app.js')
+//     .sass('resources/sass/app.scss', 'public/css');
+
+// mix.copyDirectory('node_modules/tinymce/plugins', 'public/js/tinymce/plugins');
+// mix.copyDirectory('node_modules/tinymce/themes', 'public/js/tinymce/themes');
+// mix.copyDirectory('node_modules/tinymce/icons', 'public/js/tinymce/icons');
+// mix.copyDirectory('node_modules/tinymce/skins', 'public/js/tinymce/skins');
+// mix.copyDirectory('node_modules/tinymce/models', 'public/js/tinymce/models');
