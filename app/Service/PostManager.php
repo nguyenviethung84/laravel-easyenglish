0bf8ca31eb55f2e37fef6cb1da958cc1422b11f7
hungit <hungnv@hamogroups.com>
2023-06-10 14:23:12 +0700

    api post

--- app/Service/PostManager.php
+++ app/Service/PostManager.php
@@ -6,7 +6,10 @@ use App\Models\Post;
 class PostManager{
     public static function get_latest_posts($post_type = 'EASY', $limit = 10, $offset = 1)
     {
-        $paginator = Post::where('post_type', 'LIKE', $post_type)->latest()->paginate($limit, ['*'], 'page', $offset);
+        $paginator = Post::where('post_type', 'LIKE', $post_type)
+        ->where('status', 'LIKE', 'PUBLISHED')
+        ->latest()
+        ->paginate($limit, ['*'], 'page', $offset);
         /*$paginator->count();*/
         return [
             'total_size' => $paginator->total(),
